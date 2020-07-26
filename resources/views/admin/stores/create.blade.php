5f5eef19c315e56d8c4de463476db7ccd4096e6b
luan11 <luaannovais@live.com>
2020-07-26 12:03:16 -0300

    modulo 6 concluido - gerenciamento de lojas (foco no CRUD)

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -1,42 +1,46 @@
-<h1>Criar loja</h1>
+@extends('layouts.app')
 
-<form action="/admin/stores/store" method="POST">
-    <input type="hidden" name="_token" value="{{csrf_token()}}">
-    <div>
-        <label>Nome Loja</label>
-        <input type="text" name="name">
-    </div>
+@section('content')
+    <h1>Criar loja</h1>
 
-    <div>
-        <label>Descrição</label>
-        <input type="text" name="description">
-    </div>
+    <form action="{{ route('admin.stores.store') }}" method="POST">
+        <input type="hidden" name="_token" value="{{csrf_token()}}">
+        <div class="form-group">
+            <label>Nome Loja</label>
+            <input type="text" name="name" class="form-control">
+        </div>
 
-    <div>
-        <label>Telefone</label>
-        <input type="tel" name="phone">
-    </div>
+        <div class="form-group">
+            <label>Descrição</label>
+            <input type="text" name="description" class="form-control">
+        </div>
 
-    <div>
-        <label>Celular/WhatsApp</label>
-        <input type="tel" name="mobile_phone">
-    </div>
+        <div class="form-group">
+            <label>Telefone</label>
+            <input type="tel" name="phone" class="form-control">
+        </div>
 
-    <div>
-        <label>Slug</label>
-        <input type="text" name="slug">
-    </div>
+        <div class="form-group">
+            <label>Celular/WhatsApp</label>
+            <input type="tel" name="mobile_phone" class="form-control">
+        </div>
 
-    <div>
-        <label>Usuário</label>
-        <select name="user">
-            @foreach ($users as $user)
-                <option value="{{$user->id}}">{{$user->name}}</option>
-            @endforeach
-        </select>
-    </div>
+        <div class="form-group">
+            <label>Slug</label>
+            <input type="text" name="slug" class="form-control">
+        </div>
 
-    <div>
-        <button type="submit">Criar loja</button>
-    </div>
-</form>
+        <div class="form-group">
+            <label>Usuário</label>
+            <select name="user" class="form-control">
+                @foreach ($users as $user)
+                    <option value="{{$user->id}}">{{$user->name}}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div>
+            <button type="submit" class="btn btn-success btn-lg">Criar loja</button>
+        </div>
+    </form>
+@endsection
--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -1,21 +1,30 @@
-<table>
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Store</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
+@extends('layouts.app')
 
-    <tbody>
-        @foreach ($stores as $store)
+@section('content')
+    <a href="{{ route('admin.stores.create') }}" class="btn btn-lg btn-success">Criar loja</a>
+
+    <table class="table table-striped">
+        <thead>
             <tr>
-                <td>{{ $store->id }}</td>
-                <td>{{ $store->name }}</td>
-                <td></td>
+                <th>#</th>
+                <th>Store</th>
+                <th>Actions</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+
+        <tbody>
+            @foreach ($stores as $store)
+                <tr>
+                    <td>{{ $store->id }}</td>
+                    <td>{{ $store->name }}</td>
+                    <td>
+                        <a href="{{ route('admin.stores.edit', ['store' => $store->id]) }}" class="btn btn-small btn-primary text-uppercase">Editar</a>
+                        <a href="{{ route('admin.stores.destroy', ['store' => $store->id]) }}" class="btn btn-small btn-danger text-uppercase">Remover</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 {{ $stores->links() }}
+@endsection
