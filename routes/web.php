b9a7268cbc445bd2c0242b0283250255a4d83977
luan11 <luaannovais@live.com>
2020-07-29 19:05:41 -0300

    Autenticando acesso a area admin e adicionando navbar

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     $helloWorld = 'Hello World';
 
     return view('welcome', compact('helloWorld'));
-});
+})->name('home');
 
 Route::get('/model', function () {
     /* $user = \App\User::create([
@@ -92,16 +92,24 @@ Route::get('/model', function () {
     // return \App\User::all();
 });
 
-Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function() {
-	/* Route::prefix('stores')->name('stores.')->group(function() {
-		Route::get('/', 'StoreController@index')->name('index');
-		Route::get('/create', 'StoreController@create')->name('create');
-		Route::post('/store', 'StoreController@store')->name('store');
-		Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
-        Route::post('/update/{store}', 'StoreController@update')->name('update');
-        Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
-    }); */
+Route::group([
+    'middleware' => ['auth']
+], function() {
+    Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function() {
+        /* Route::prefix('stores')->name('stores.')->group(function() {
+            Route::get('/', 'StoreController@index')->name('index');
+            Route::get('/create', 'StoreController@create')->name('create');
+            Route::post('/store', 'StoreController@store')->name('store');
+            Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
+            Route::post('/update/{store}', 'StoreController@update')->name('update');
+            Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
+        }); */
 
-    Route::resource('stores', 'StoreController');
-    Route::resource('products', 'ProductController');
+        Route::resource('stores', 'StoreController');
+        Route::resource('products', 'ProductController');
+    });
 });
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
